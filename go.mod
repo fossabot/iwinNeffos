62ed11bca6f0e0394d52cbda4323fb5de87c30ae
majid bigdeli <majidbigdeli72@gmail.com>
2019-06-04 02:38:59 +0430

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/majidbigdeli/neffosAmi
+
+require (
+	github.com/ivahaev/amigo v0.1.9
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
+	github.com/kataras/neffos v0.0.0-20190603085547-9fba63259400
+	github.com/spf13/viper v1.4.0
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ivahaev/amigo"
+	"github.com/kardianos/osext"
+	"github.com/kataras/neffos"
+	"github.com/kataras/neffos/gobwas"
+	"github.com/spf13/viper"
+)
+
+type asteriskConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+}
+
+var (
+	a        *amigo.Amigo
+	c        *neffos.NSConn
+	err      error
+	endpoint string
+)
+
+const (
+	namespace = "default"
+)
+
+func varSetHandler(e map[string]string) {
+	switch e["Variable"] {
+	case "__ExtCallId":
+		{
+			fmt.Println("__ExtCallId :", e["Value"])
+			_ = c.Emit("send", []byte(e["Value"]))
+		}
+	}
+}
+
+var handler = neffos.Namespaces{
+	namespace: neffos.Events{
+		neffos.OnNamespaceConnected: func(c *neffos.NSConn, msg neffos.Message) error {
+			log.Printf("[%s] connected to namespace [%s].", c, msg.Namespace)
+			return nil
+		},
+		neffos.OnNamespaceDisconnect: func(c *neffos.NSConn, msg neffos.Message) error {
+			log.Printf("[%s] disconnected from namespace [%s].", c, msg.Namespace)
+			return nil
+		},
+	},
+}
+
+func init() {
+
+	path, err := osext.ExecutableFolder()
+
+	if err != nil {
+		panic(fmt.Errorf("Fatal error ExecutableFolder: %s", err.Error()))
+	}
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(path)
+	err = viper.ReadInConfig() // Find and read the config file
+	if err != nil {            // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s", err.Error()))
+	}
+}
+
+func main() {
+
+	var (
+		dialer = gobwas.Dialer(gobwas.Options{Header: gobwas.Header{"X-Username": []string{"AMI"}}})
+	
+	)
+	endpoint = viper.GetString("socket.endpoint")
+
+
+	//get asterisk config
+	var ast asteriskConfig
+	if err := viper.UnmarshalKey("asterisk", &ast); err != nil {
+		panic(err)
+	}
+	settings := &amigo.Settings{Host: ast.Host, Port: ast.Port, Username: ast.Username, Password: ast.Password}
+
+	a = amigo.New(settings)
+	a.Connect()
+
+	err = a.RegisterHandler("VarSet", varSetHandler)
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error VarSet: %s", err.Error()))
+	}
+
+	client(dialer)
+
+
+	ch := make(chan bool)
+	<-ch
+}
+
+func client(dialer neffos.Dialer) {
+
+	client, err := neffos.Dial(nil, dialer, endpoint, handler)
+	if err != nil {
+		log.Print(err)
+	}
+	//defer client.Close()
+	c, err = client.Connect(nil, namespace)
+	if err != nil {
+		panic(err)
+	}
+}
